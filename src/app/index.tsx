9acdaca6abad26da4358b4f6189b54ce47650686
Ritankar Bhattacharjee <ritankar.bhattacharjee80@gmail.com>
2025-09-02 22:30:52 +0530

    initialised project and set up nativewind

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 flex items-center justify-center">
+      <Text className="text-amber-900">Budget Crafter</Text>
+    </View>
+  );
+}
