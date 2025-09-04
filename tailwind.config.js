aef398bd2ca58c241c9d31e9f336123825a1282b
Ritankar Bhattacharjee <ritankar.bhattacharjee80@gmail.com>
2025-09-04 21:54:55 +0530

    added fonts to tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,13 +14,6 @@ module.exports = {
       colors: {
         error: "#F14141",
         success: "#2F9B65",
-        gray: {
-          100: "#878787",
-          200: "#878787",
-        },
-        dark: {
-          100: "#181C2E",
-        },
       },
     },
   },
